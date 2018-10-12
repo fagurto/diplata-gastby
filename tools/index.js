a3ae2d5330b18b5cc541eddb09b92c32ec40ee46
Francisco Agurto <fagurto@cplt.cl>
2018-10-12 12:26:15 -0300

    data viernes

--- tools/index.js
+++ tools/index.js
@@ -3,25 +3,35 @@ const jsonfile = require("jsonfile");
 const lineReader = new LineByLineReader("../data/diplata-products.jsonlp");
 const makeDir = require("make-dir");
 const del = require("del");
+/*const gitP = require('simple-git/promise');
+const gitS = require('simple-git/promise')();
+const git = gitP('../');
 
+git.status().then(status => { 
 
-del(["../data/dataset/**/*.json"], { force: true }).then(paths => {
-   lineReader.on("line", line => {
+     console.log(status);
+ });*/
 
-      const jsonObject = JSON.parse(line);
+ 
 
-      const { category, hash } = jsonObject;
-
-      const dir = "../data/dataset/" + category + "/";
-
-      makeDir(dir).then(path => {
-      
-         const fileName = path + "/" + hash + ".json";
-         jsonfile.writeFile(fileName, jsonObject, function(err) {
-            if (err) console.error(err);
-         });
-      });
-   });
-});
+ del(["../data/dataset/**/*.json"], { force: true }).then(paths => {
+    lineReader.on("line", line => {
+ 
+       const jsonObject = JSON.parse(line);
+ 
+       const { category, hash } = jsonObject;
+ 
+       const dir = "../data/dataset/" + category + "/";
+ 
+       makeDir(dir).then(path => {
+       
+          const fileName = path + "/" + hash + ".json";
+          jsonfile.writeFile(fileName, jsonObject, function(err) {
+             if (err) console.error(err);
+          });
+       });
+    });
+ });
+ 
 
 
